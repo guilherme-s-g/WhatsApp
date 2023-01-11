7d0fd4df7f6e1c61eb3bc686189cab51def4b741
Guilherme Sabino <59611598+guilherme-s-g@users.noreply.github.com>
2023-01-11 08:03:58 -0300

    Ajuste
    Ajustes de tela

--- app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
@@ -3,12 +3,8 @@ package com.example.whatsapp.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.whatsapp.R;
 import com.example.whatsapp.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity
@@ -28,11 +24,6 @@ public class ChatActivity extends AppCompatActivity
         setSupportActionBar(binding.toolbar);
         binding.toolbar.setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_chat);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
     }
 
 }
