fa7843b4ef46d85ec8ae3d1218d60307c802e884
Guilherme S <sabino919191@gmail.com>
2023-09-22 12:18:39 -0300

    Criação do adapter de listagem de mensagens

--- app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
@@ -2,15 +2,29 @@ package com.example.whatsapp.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.whatsapp.R;
+import com.example.whatsapp.adapter.MensagensAdapter;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
 import com.example.whatsapp.databinding.ActivityChatBinding;
+import com.example.whatsapp.helper.Base64Custom;
+import com.example.whatsapp.helper.UsuarioFirebase;
+import com.example.whatsapp.model.Mensagem;
 import com.example.whatsapp.model.Usuario;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,6 +37,16 @@ public class ChatActivity extends AppCompatActivity
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
+    private EditText editMensagem;
+
+    //identificador usuarios remetente e destinatario
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
+
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter adapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
+
 
 
     @Override
@@ -40,6 +64,11 @@ public class ChatActivity extends AppCompatActivity
         //Configurações iniciais
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFoto);
+        editMensagem = findViewById(R.id.editMensagem);
+        recyclerMensagens = findViewById(R.id.recyclerMensagens);
+
+        //recupera dados do usuario remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //Recuperar dados do usuário destinatario
         Bundle bundle = getIntent().getExtras();
@@ -59,8 +88,54 @@ public class ChatActivity extends AppCompatActivity
              {
                  circleImageViewFoto.setImageResource(R.drawable.padrao);
              }
+
+             //recuperar dados usuario destinatario
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
 
+        //Configuração adapter
+        adapter = new MensagensAdapter(mensagens, getApplicationContext());
+
+
+
+        //Configuração recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(adapter);
+
+    }
+
+    public void enviarMensagem(View view)
+    {
+        String textoMensagem = editMensagem.getText().toString();
+
+        if (!textoMensagem.isEmpty())
+        {
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            //Salvar mensagem para o remetente
+            salvarMensagem(idUsuarioRemetente,idUsuarioDestinatario, mensagem);
+        }
+        else
+        {
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg)
+    {
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+        editMensagem.setText("");
     }
 
 }
